4d35450b459a53ba330095ff3b7af16e3f0df078
shdwrv <shdwrv@gmail.com>
2022-09-02 23:20:23 +0200

    Add project files.

---
+++ TypewriterExample.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace typewriter
+{
+    partial class TypewriterExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This is a test typewriter label.\r\nAnd this is the second line.\r\nText will be writ" +
+    "ten at the delay specified below.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Write!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "100";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 57);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(265, 49);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "This is a test typewriter label.\r\nAnd this is the second line.\r\nText will be writ" +
+    "ten at the delay specified below.\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 118);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TypewriterExample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ TypewriterExample.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace typewriter
+{
+    public partial class TypewriterExample : Form
+    {
+        public TypewriterExample()
+        {
+            InitializeComponent();
+        }
+
+        private void Typewriter(string text, int delay)
+        {
+            // Typewriter function by shdwrv
+            // Call example:
+            // private void button1_Click(object sender, EventArgs e)
+            // {
+            //     Typewriter(label1.Text, 100);
+            // }
+
+            void Wait(int milliseconds)
+            {
+                // Wait function
+                // Taken from: https://stackoverflow.com/a/52906286
+
+                var timer = new System.Windows.Forms.Timer();
+
+                timer.Interval = milliseconds;
+                timer.Enabled = true;
+                timer.Start();
+
+                timer.Tick += (s, e) =>
+                {
+                    timer.Enabled = false;
+                    timer.Stop();
+                };
+
+                while (timer.Enabled)
+                {
+                    Application.DoEvents();
+                }
+            }
+            int length = text.Length;   // Length of text, to measure loops.
+            string written = "";        // Already written text.
+            string currentletter = "";  // Current letter to append written text with
+            int currentlength = 0;      // Current letter index.
+
+            do
+            {
+                currentletter = text[currentlength].ToString();     // Current letter equals current index, string.
+                if (currentletter != " ")                           // If current letter is space, do not wait ( optional, i thought it looks better )
+                    Wait(delay);                                    // Utilizing previously defined wait, for the next letter.
+                written = written + currentletter;                  // Written text appended by current letter.
+
+                currentlength = currentlength + 1;                  // Current index increases by one.
+                Console.Write(written);                             // Logging current text.
+                // If you want the typewriter to target an element, like a label, replace line above with:
+                label1.Text = written;
+            }
+            while (length != currentlength);                        // Repeat until length is equal current letter.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Typewriter(textBox2.Text, Convert.ToInt32(textBox1.Text));
+        }
+    }
+}
